5d8f92e4c33ccf12891d8b57c2e66db183c45292
dev@127.4.7.8 <devsekarindhu18@gmail.com>
2024-12-07 14:35:03 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,17 +1,10 @@
 @echo off
-:: Set the URL and output file name
-set "url=https://raw.githubusercontent.com/Dev-0618/127.4.7.8/main/DEV_RESUME.pdf"
-set "output=DEV_RESUME.pdf"
+setlocal enabledelayedexpansion
 
-:: Use curl to download the file
-curl -o "%output%" "%url%"
-
-:: Check if the file was downloaded successfully
-if exist "%output%" (
-    echo File downloaded successfully: %output%
-    start "" "%output%"
-) else (
-    echo Failed to download the file.
+for /L %%i in (1,1,20) do (
+    start mshta "javascript:alert('Hi, your Hacked dev@127.4.7.8');close();"
+    timeout /nobreak /t 0 >nul
 )
 
-pause
+timeout /t 5 >nul
+exit
